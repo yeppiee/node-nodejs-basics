d8d188d7f5c89aa7bb9be62c2a3a87a754443199
yeppiee <pakhotnykh.i@gmail.com>
2024-10-02 20:15:41 +0300

    feat: implement fs:copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,15 @@
+import { cp } from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await cp("src/fs/files", "src/fs/files_copy", {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    });
+  } catch (error) {
+    throw Error("FS operation failed");
+  }
 };
 
 await copy();
