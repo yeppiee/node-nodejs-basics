8ba8cf2046b9bab12dcab3ddd8cd2d5fbb3f0070
yeppiee <pakhotnykh.i@gmail.com>
2024-10-03 20:46:31 +0300

    feat: implement fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { readdir, stat } from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const allFilesFolderContent = await readdir("src/fs/files", {
+    withFileTypes: true,
+  }).catch(() => {
+    throw Error("FS operation failed");
+  });
+
+  const onlyFiles = allFilesFolderContent.filter((file) => file.isFile());
+
+  const onlyFilesNames = onlyFiles.map((file) => file.name.split(".")[0]);
+
+  console.log(onlyFilesNames);
 };
 
 await list();
