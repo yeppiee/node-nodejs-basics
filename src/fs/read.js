e68bb754c91676cf6aaaca8b038d4ea3edbffad1
yeppiee <pakhotnykh.i@gmail.com>
2024-10-05 17:35:12 +0300

    feat: implement fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readFile } from "node:fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile("src/fs/files/fileToRead.txt", "utf8").catch(
+      () => {
+        throw Error("FS operation failed");
+      }
+    );
+    console.log(content);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await read();
