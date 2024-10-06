6d6d884a19d3ba98c2c307194edd02ba435c85e8
yeppiee <pakhotnykh.i@gmail.com>
2024-10-06 17:17:12 +0300

    feat: implement zip:decompress, create fileToCompress with decompression content

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { rm, stat } from "node:fs/promises";
+import { createGunzip } from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    await stat("src/zip/files/archive.gz").catch(() => {
+      throw Error("Sorry, but archive.gz is not defined");
+    });
+
+    const readStream = createReadStream("src/zip/files/archive.gz");
+    const writeStream = createWriteStream("src/zip/files/fileToCompress.txt");
+
+    readStream.pipe(createGunzip()).pipe(writeStream);
+
+    await rm("src/zip/files/archive.gz");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await decompress();
