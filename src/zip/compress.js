aac54a7198c522c0fcfc6ab0da58a00c5e6379c8
yeppiee <pakhotnykh.i@gmail.com>
2024-10-06 17:15:35 +0300

    feat: delete fileToCompress after compression

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,11 +1,22 @@
 import { createReadStream, createWriteStream } from "node:fs";
+import { rm, stat } from "node:fs/promises";
 import { createGzip } from "node:zlib";
 
 const compress = async () => {
-  const readStream = createReadStream("src/zip/files/fileToCompress.txt");
-  const writeStream = createWriteStream("src/zip/files/archive.gz");
+  try {
+    await stat("src/zip/files/fileToCompress.txt").catch(() => {
+      throw Error("Sorry, but fileToCompress.txt is not defined");
+    });
 
-  readStream.pipe(createGzip()).pipe(writeStream);
+    const readStream = createReadStream("src/zip/files/fileToCompress.txt");
+    const writeStream = createWriteStream("src/zip/files/archive.gz");
+
+    readStream.pipe(createGzip()).pipe(writeStream);
+
+    await rm("src/zip/files/fileToCompress.txt");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await compress();
