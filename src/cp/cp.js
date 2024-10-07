3d4f35234dc90db2814341c9dbffd3e83e3b89d7
yeppiee <pakhotnykh.i@gmail.com>
2024-10-07 19:41:33 +0300

    feat: enable ipc in cp spawn method

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,7 +6,9 @@ const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const scriptPath = path.join(__dirname, "files/script.js");
 
 const spawnChildProcess = async (args) => {
-  const child = spawn("node", [scriptPath, ...args]);
+  const child = spawn("node", [scriptPath, ...args], {
+    stdin: ["inherit", "inherit", "inherit", "ipc"],
+  });
 
   process.stdin.pipe(child.stdin);
 
